8364c4a1175c5ab80d058dc1be8343037553402e
italocunhabueno <italohellboy@gmail.com>
2021-04-16 08:52:41 -0300

    Update

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
-from spellchecker import SpellChecker
+from tqdm import tqdm
+from time import sleep
 
-spell = SpellChecker('pt')  # loads default word frequency list
-spell.word_frequency.load_text_file(r'C:\Users\Italo\Desktop\palavras.txt')
-
-palavra = "Meu nome é Italo e eu tenho apitidao para screver python".split(" ")
-misspelled = spell.unknown(palavra)
-
-for word in misspelled:
-    print("{0} =====> {1}".format(word, spell.correction(word)))
-
-    print("Candidatos: {0}".format(spell.candidates(word)))
-    print()
+for _ in tqdm(range(1000)):
+    sleep(0.001)
+print('concluido')
