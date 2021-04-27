c1225dead01559f5dde60f754296bb9210a474fe
italocunhabueno <italohellboy@gmail.com>
2021-04-27 16:17:01 -0300

    Update

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
+from os.path import dirname, join
+from os import walk
 from tqdm import tqdm
-from time import sleep
+from time import time
 
-for _ in tqdm(range(1000)):
-    sleep(0.001)
-print('concluido')
+COMECO = time()
+PASTA = r'\\10.0.0.21\maverick'
+
+PATHS = []
+FILES = [join(root, f) for root, dirs, files in walk(PASTA) for f in files if 'Ladybug_Panoramic' in root and f.endswith('.jpg')]
+for F in tqdm(FILES, desc="ANALISANDO ARQUIVOS...", colour='green'):
+    CAMINHO = dirname(F)
+    if dirname(F) not in PATHS:
+        PATHS.append(dirname(F))
+
+for P in PATHS:
+    print(P)
+
+print("")
+print("Quantidade de arquivos: {0}".format(len(FILES)))
+print("TEMPO: {0}".format(time()-COMECO))
