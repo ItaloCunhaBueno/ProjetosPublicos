fb13e1de7751810c03d162068b4e2d7292a8d0ae
italocunhabueno <italohellboy@gmail.com>
2021-05-24 08:31:39 -0300

    Update

--- test.py
+++ test.py
@@ -4,21 +4,20 @@ from os import chdir, path
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import threading
 
+
 def startserver():
-    if getattr(sys, 'frozen', False):
+    if getattr(sys, "frozen", False):
         RUNPATH = path.dirname(sys.executable)
     elif __file__:
         RUNPATH = path.dirname(__file__)
     chdir(RUNPATH)
-    httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+    httpd = HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
     httpd.serve_forever()
 
-Thread1 = threading.Thread(target=startserver)
-Thread1.daemon = True
-Thread1.start()
-webview.create_window('WEBMAP', 'http://localhost:8000')
-webview.start(http_server=True, gui="cef")
 
-
-
-
+if __name__ == "__main__":
+    Thread1 = threading.Thread(target=startserver)
+    Thread1.daemon = True
+    Thread1.start()
+    webview.create_window("WEBMAP", "http://localhost:8000")
+    webview.start(http_server=True, gui="cef")
