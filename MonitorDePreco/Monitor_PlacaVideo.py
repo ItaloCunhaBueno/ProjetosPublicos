72d4c6eeab85638767740bf00158d6e1b8770f6c
italocunhabueno <italohellboy@gmail.com>
2021-08-19 09:25:01 -0300

    Update

--- MonitorDePreco/Monitor_PlacaVideo.py
+++ MonitorDePreco/Monitor_PlacaVideo.py
@@ -1,37 +1,95 @@
-import requests as rq
-from bs4 import BeautifulSoup as bs
-import json
-import re
+from requests_html import HTMLSession
 
-SITE = 'https://www.kabum.com.br/hardware/placa-de-video-vga/nvidia/geforce-rtx'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-DATA = rq.get(SITE, headers=headers)
-SOUP = bs(DATA.content, "lxml")
-SCRIPTS = SOUP.find_all('script', type='text/javascript')
+print("ANALISANDO KABUM E TERABYTESHOP...")
 
-PRECOS = []
-PRECODESCONTO = []
-PRODUTO = []
-for S in SCRIPTS:
-    json_string = re.search(r'listagemDados\s*=\s*(.*?}])\s*\n', str(S), flags=re.DOTALL)
-    if json_string:
-        json_data = json.loads(json_string[1])
-        for D in json_data:
-            #pprint(D)
-            if '30' in D['nome'] and D['disponibilidade'] is True:
-                PRECOS.append(D['preco'])
-                PRECODESCONTO.append(D['preco_desconto'])
-                PRODUTO.append(D['nome'])
+KABUM = "https://kabum.com.br/hardware/placa-de-video-vga/nvidia/geforce-rtx?page_number=1&page_size=20&facet_filters=&sort=price"
+TERABYTE = "https://www.terabyteshop.com.br/hardware/placas-de-video/nvidia-geforce"
 
-BARATO = min(PRECOS)
-PBARATO = PRODUTO[PRECOS.index(BARATO)]
-BARATODESC = PRECODESCONTO[PRECOS.index(BARATO)]
+session = HTMLSession(browser_args=["--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.68"])
+pagina = session.get(KABUM)
+pagina.html.render(scrolldown=10, sleep=1)
 
-print('='*100)
-print('PRODUTO MAIS BARATO HOJE:')
-print()
-print(f'Nome: {PBARATO}')
-print(f'Preço: {BARATO}')
-print(f'Preço Boleto: {BARATODESC}')
-print()
-input('Precione ENTER para sair...')
+LISTA60_TITULO = []
+LISTA60_PRECO = []
+LISTA60_LINK = []
+
+LISTA70_TITULO = []
+LISTA70_PRECO = []
+LISTA70_LINK = []
+
+ELEMENT = pagina.html.find("main")
+for E in ELEMENT:
+    if "id" in E.attrs:
+        if E.attrs["id"] == "listing":
+            DIV = E.find("div")
+            for D in DIV:
+                if "class" in D.attrs:
+                    if "productCard" in D.attrs["class"]:
+                        CARD = D.find("a")[0]
+                        TEXTO = CARD.text.split("\n")
+                        TITULO = TEXTO[1]
+                        PRECO = float("".join([p for p in TEXTO[2] if p in "0123456789,"]).replace(",", "."))
+                        LINK = list(CARD.absolute_links)[0]
+
+                        if "3060" in TITULO:
+                            LISTA60_TITULO.append(TITULO)
+                            LISTA60_PRECO.append(PRECO)
+                            LISTA60_LINK.append(LINK)
+
+                        if "3070" in TITULO:
+                            LISTA70_TITULO.append(TITULO)
+                            LISTA70_PRECO.append(PRECO)
+                            LISTA70_LINK.append(LINK)
+
+pagina = session.get(TERABYTE)
+pagina.html.render(scrolldown=10, sleep=1)
+ELEMENT = pagina.html.find("div")
+for E in ELEMENT:
+    if "id" in E.attrs:
+        if E.attrs["id"] == "prodarea":
+            DIVS = E.find("div")
+            for D in DIVS:
+                if "class" in D.attrs:
+                    if "commerce_columns_item_inner" in D.attrs["class"]:
+                        DIVS2 = D.find("div")
+                        for D2 in DIVS2:
+                            if "class" in D2.attrs:
+                                if "commerce_columns_item_caption" in D2.attrs["class"]:
+                                    TITULO = D2.text
+                                    LINK = list(D2.absolute_links)[0]
+                                if "commerce_columns_item_info" in D2.attrs["class"]:
+                                    DIVS3 = D2.find("div")
+                                    for D3 in DIVS3:
+                                        if "class" in D3.attrs:
+                                            if "prod-new-price" in D3.attrs["class"]:
+                                                PRECO = float("".join([p for p in D3.text if p in "0123456789,"]).replace(",", "."))
+                                                if PRECO:
+                                                    if "3060" in TITULO:
+                                                        LISTA60_TITULO.append(TITULO)
+                                                        LISTA60_PRECO.append(PRECO)
+                                                        LISTA60_LINK.append(LINK)
+
+                                                    if "3070" in TITULO:
+                                                        LISTA70_TITULO.append(TITULO)
+                                                        LISTA70_PRECO.append(PRECO)
+                                                        LISTA70_LINK.append(LINK)
+
+MIN60 = min(LISTA60_PRECO)
+ID60 = LISTA60_PRECO.index(MIN60)
+
+MIN70 = min(LISTA70_PRECO)
+ID70 = LISTA70_PRECO.index(MIN70)
+
+print("")
+print("MENOR PREÇO 3060:")
+print(LISTA60_TITULO[ID60])
+print(LISTA60_PRECO[ID60])
+print(LISTA60_LINK[ID60])
+print("")
+
+print("MENOR PREÇO 3070:")
+print(LISTA70_TITULO[ID70])
+print(LISTA70_PRECO[ID70])
+print(LISTA70_LINK[ID70])
+print("")
+input('Pressione ENTER para sair...')
